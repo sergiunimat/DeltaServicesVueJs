b92e722250440eb42040f274c94b7ded62389c98
Sergiu Nimat 👑 <nimat.sergiu@hotmail.com>
2025-01-26 18:57:52 +0100

    Setup router & remove unused code

---
+++ casino-lobby/src/components/HomePage/HomePage.vue
@@ -0,0 +1,11 @@
+﻿<script setup>
+
+</script>
+
+<template>
+<h2>home page</h2>
+</template>
+
+<style scoped>
+
+</style>
--- casino-lobby/src/main.js
+++ casino-lobby/src/main.js
@@ -1,4 +1,19 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
+import {createRouter, createWebHistory} from "vue-router";
+import HomePage from "@/components/HomePage/HomePage.vue";
+import GameInstance from "@/components/Game/GameInstance.vue";
 
-createApp(App).mount('#app')
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {path: '/', component: HomePage},
+        {path: '/home', component: HomePage},
+        {path: '/game/:gameId', component: GameInstance},
+    ]
+})
+
+const app = createApp(App)
+app.use(router)
+
+app.mount('#app')
